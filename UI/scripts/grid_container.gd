fc79c9b04b408c0d0a707b5008e750166d66775b
eytk08 <sphoebekate9@gmail.com>
2025-06-11 00:31:40 +0800

    Initial commit

--- UI/scripts/grid_container.gd
+++ UI/scripts/grid_container.gd
@@ -3,8 +3,8 @@ extends GridContainer
 func _ready():
 	
 	columns = 3  # Set to your column count
-	add_theme_constant_override("hseparation", 20)  # Horizontal spacing
-	add_theme_constant_override("vseparation", 20)  # Vertical spacing
+	add_theme_constant_override("hseparation", 150)  # Horizontal spacing
+	add_theme_constant_override("vseparation", 150)  # Vertical spacing
 	
 	var placeholder_scene = preload("res://UI/scenes/UICardPlaceholder.tscn")
 	
